49e1ce3fe3e2a4db243e59f678545aee9e52fc8e
Gerald Bauer <gerald.bauer@gmail.com>
2015-07-25 11:59:05 +0200

    up

--- Rakefile
+++ Rakefile
@@ -2,21 +2,60 @@
 
 
 require './scripts/html2txt'
+require './scripts/schedule'
 
+
+YEAR_TO_SEASON =
+{
+  2011 => '2010-11',
+  2012 => '2011-12',
+  2013 => '2012-13',
+  2014 => '2013-14',
+  2015 => '2014-15',
+  2016 => '2015-16',
+}
 
 
 ENG_BASE = 'http://rsssf.com/tablese'
-ENG      = %w[eng2011 eng2012 eng2013 eng2014 eng2015]
-## ENG      = %w[eng2011]
+ENG      = [2011, 2012, 2013, 2014, 2015]
+## ENG      = [2011]
 ## e.g. http://rsssf.com/tablese/eng2015.html
 
 ENG_REPO = '../en-england'
 
 
+## Premiership  in 2011,2012, 2013
+## Premier League in 2014, 2015
+##
+## todo/fix:
+### reading >../en-england/tables/eng2014.txt<
+##  includes  invalid byte sequence in UTF-8 !!!
+
+
+task :eng2 do
+  ENG.each do |year|
+    src_txt = "#{ENG_REPO}/tables/eng#{year}.txt"
+    puts "  reading >#{src_txt}<"
+    txt   = File.read( src_txt )
+    txt.force_encoding( 'ASCII-8BIT' )  ## fix: check for chars > 127 (e.g. not 7-bit)
+    ## pp txt
+    schedule = find_schedule( txt, 'Premiership|Premier League' )
+    pp schedule
+
+    dest_path = "#{ENG_REPO}/#{YEAR_TO_SEASON[year]}/1-premierleague.txt"
+    puts "  save to >#{dest_path}<"
+    FileUtils.mkdir_p( File.dirname( dest_path ))
+    File.open( dest_path, 'w' ) do |f|
+      f.write schedule
+    end
+  end
+end
+
+
 task :eng do
 
   ENG.each do |name|
-    src_url = "#{ENG_BASE}/#{name}.html"
+    src_url = "#{ENG_BASE}/eng#{year}.html"
     html  = fetch( src_url )
     txt   = html_to_txt( html )
 
@@ -28,7 +67,7 @@ task :eng do
 
 EOS
 
-    dest_path = "#{ENG_REPO}/tables/#{name}.txt"
+    dest_path = "#{ENG_REPO}/tables/eng#{year}.txt"
     File.open( dest_path, 'w' ) do |f|
       f.write header
       f.write txt
---
+++ scripts/schedule.rb
@@ -0,0 +1,68 @@
+# encoding: utf-8
+
+
+def find_schedule( txt, header )
+
+  ## find match schedule/fixtures in multi-league doc
+  new_txt = ''
+
+  ## stages
+  league_header_found        = false
+  first_round_header_found   = false
+  round_header_found         = false
+
+
+  txt.each_line do |line|
+    
+    if league_header_found == false
+      ## first find start of league header/section
+      ## assumes heading 4 for now
+      
+      if line =~ /####\s+(#{header})/i
+        puts "!!! bingo - found header >#{line}<"
+        league_header_found = true
+        new_txt << line
+        new_txt << "\n"
+      end
+    elsif first_round_header_found == false
+      ## next look for first round (starting w/ Round)
+      if line =~ /Round/i
+        puts "!!! bingo - found first round >#{line}<"
+        first_round_header_found = true
+        round_header_found       = true
+        new_txt << line
+      elsif line =~ /=-=-=-=/
+        puts "*** no rounds found; hit section marker (horizontal rule)"
+        break
+      else
+        next ## continue; searching
+      end
+    elsif round_header_found == true
+      ## collect rounds;
+      ##   assume text block until next blank line
+      ##   new block must allways start w/ round
+      if line =~ /^\s*$/   ## blank line?
+        round_header_found = false  
+        new_txt << line
+      else
+        new_txt << line   ## keep going until next blank line
+      end
+    else
+      ## skip (more) blank lines
+      if line =~ /^\s*$/
+        next  ## continue; skip extra blank line
+      elsif line =~ /Round/i
+        puts "!!! bingo - found new round >#{line}<"
+        round_header_found = true   # more rounds; continue
+        new_txt << line
+      else
+        ## if we get here
+        ##   end of match schedule
+        puts "!!! stop schedule before line >#{line}<"
+        break
+      end
+    end
+  end  # each line
+
+  new_txt
+end  # method find_schedule
